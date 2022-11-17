ac57dfdd744f35de6332d2450131708f13244cf6
William Benham <william.benham77@gmail.com>
2022-11-17 11:44:18 -0700

    Initial Commit - Models started

---
+++ WMS_Inventory_API_Client/Models/Account.cs
@@ -0,0 +1,39 @@
+﻿using static System.Collections.Specialized.BitVector32;
+using System.Diagnostics.Metrics;
+using System.Reflection;
+
+namespace WMS_Inventory_API_Client.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Address1 { get; set; }
+        public string? Address2 { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public int? ZipCode { get; set; }
+        public virtual List<StorageLocation>? storageLocations { get; set; }
+
+        public Account(int? id, string name, string address1, string? address2, string city, string state, int zipCode, string? construction, string? rodimage, int makerid, Maker maker)
+        {
+            Id = id;
+            Model = model;
+            LengthFeet = lengthfeet;
+            Sections = sections;
+            LineWeight = lineweight;
+            YearMade = yearmade;
+            Type = type;
+            Construction = construction;
+            RodImage = rodimage;
+            MakerId = makerid;
+            Maker = maker;
+        }
+
+        public Account()
+        {
+            return;
+        }
+
+    }
+}
