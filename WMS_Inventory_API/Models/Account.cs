e0525dcb1dc6a52662f1e05e5ccf8cc3f0e631c6
William Benham <william.benham77@gmail.com>
2022-11-16 18:56:04 -0700

    Initial Commit - Models & DB Context

---
+++ WMS_Inventory_API/Models/Account.cs
@@ -0,0 +1,14 @@
+﻿namespace WMS_Inventory_API.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Address1 { get; set; }
+        public string? Address2 { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public int? ZipCode { get; set; }
+        public virtual List<StorageLocation>? storageLocations { get; set; }
+    }
+}
