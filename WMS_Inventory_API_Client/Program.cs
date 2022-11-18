4e5cbd037fa0f7adaf1698967bf98e59864d83cd
Gage-Stephens <stephegns@gmail.com>
2022-11-18 10:50:39 -0500

    changes

--- WMS_Inventory_API_Client/Program.cs
+++ WMS_Inventory_API_Client/Program.cs
@@ -4,34 +4,50 @@ namespace WMS_Inventory_API_Client
 {
     public class Program
     {
-        public static ServiceDescriptor? videoGame { get; private set; }
+        public static ServiceDescriptor? account { get; private set; }
+        public static ServiceDescriptor? content { get; private set; }
+        public static ServiceDescriptor? container { get; private set; }
+        public static ServiceDescriptor? storageLocation { get; private set; }
+       
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            // Add services to the container.
+            // Add services to the container
             builder.Services.AddControllersWithViews();
 
-            // \/ Add one of these for each Interface/Service \/
-            builder.Services.AddHttpClient<IAccountService, AccountService>(c =>
+            builder.Services.AddHttpClient<IAccountService, AccountService>(c=>
+            c.BaseAddress = new Uri("https://localhost:7256/"));
+            
+            builder.Services.AddHttpClient<IContentService, ContentService>(c=>
+            c.BaseAddress = new Uri("https://localhost:7256/"));
+            
+            builder.Services.AddHttpClient<IContainerService, ContainerService>(c=>
             c.BaseAddress = new Uri("https://localhost:7256/"));
 
-
+            builder.Services.AddHttpClient<IStorageLocationService, StorageLocationService>(c=>
+            c.BaseAddress = new Uri("https://localhost:7256/"));
+            
             var app = builder.Build();
             // Configure the HTTP request pipeline.
             
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
             app.UseHttpsRedirection();
+
             app.UseStaticFiles();
+
             app.UseRouting();
+
             app.UseAuthorization();
+
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
+                
             app.Run();
         }
     }
