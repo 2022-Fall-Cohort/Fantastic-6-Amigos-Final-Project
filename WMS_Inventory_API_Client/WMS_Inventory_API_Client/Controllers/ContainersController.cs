33b43bc5caeb1739177fc9e8dffbabc16a2fa404
William Benham <william.benham77@gmail.com>
2022-11-22 19:59:42 -0700

    Extended Content, added ViewBag, modified Views

---
+++ WMS_Inventory_API_Client/WMS_Inventory_API_Client/Controllers/ContainersController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using WMS_Inventory_API_Client.Models;
+using WMS_Inventory_API_Client.Services.Interfaces;
+
+namespace WebMVC_API_Client.Controllers
+{
+    public class ContainerController : Controller
+    {
+        private IContainerService? _service;
+        private IStorageLocationService? _serviceStorageLocation;
+
+        private static readonly HttpClient client = new HttpClient();
+
+        private string requestUri = "https://localhost:7153/api/Containers/";
+
+        public ContainerController(IContainerService service, IStorageLocationService serviceStorageLocation)
+        {
+            _service = service ?? throw new ArgumentNullException(nameof(service));
+            _serviceStorageLocation = serviceStorageLocation ?? throw new ArgumentNullException(nameof(serviceStorageLocation));
+
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            client.DefaultRequestHeaders.Add("User-Agent", "Jim's API");
+        }
+
+        // Example: https://localhost:7153/api/Containers
+        public async Task<IActionResult> Index()
+        {
+            var response = await _service.FindAll();
+
+            return View(response);
+        }
+
+        // GET: Container/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var container = await _service.FindOne(id);
+            if (container == null)
+            {
+                return NotFound();
+            }
+
+            return View(container);
+        }
+
+        // GET: Container/Create
+        public async Task<IActionResult> Create()
+        {
+            var response = await _serviceStorageLocation.FindAll();
+            ViewData["StorageLocationId"] = new SelectList(response, "Id", "LocationName");
+            return View();
+        }
+
+        // POST: Container/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("Id,Type,Description,StorageLocationID")] Container container)
+        public async Task<IActionResult> Create(Container container)
+        {
+            container.Id = null;
+            var resultPost = await client.PostAsync<Container>(requestUri, container, new JsonMediaTypeFormatter());
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Container/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var response = await _serviceStorageLocation.FindAll();
+            ViewData["StorageLocationId"] = new SelectList(response, "Id", "LocationName");
+
+            var container = await _service.FindOne(id);
+            if (container == null)
+            {
+                return NotFound();
+            }
+
+            return View(container);
+        }
+
+        // POST: Container/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("Id,Type,Description,StorageLocationID,StorageLocation")] Container container)
+        public async Task<IActionResult> Edit(int id, Container container)
+        {
+            if (id != container.Id)
+            {
+                return NotFound();
+            }
+
+            var resultPut = await client.PutAsync<Container>(requestUri + container.Id.ToString(), container, new JsonMediaTypeFormatter());
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Container/Delete/5
+        public async Task<IActionResult> Delete(int id)
+
+        {
+            var container = await _service.FindOne(id);
+            if (container == null)
+            {
+                return NotFound();
+            }
+
+            return View(container);
+        }
+
+        // POST: Container/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var resultDelete = await client.DeleteAsync(requestUri + id.ToString());
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ WMS_Inventory_API_Client/WMS_Inventory_API_Client/Controllers/ContentsController.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using WMS_Inventory_API_Client.Models;
+using WMS_Inventory_API_Client.Services.Interfaces;
+
+namespace WMS_Inventory_API_Client.Controllers
+{
+    public class ContentsController : Controller
+    {
+        private IContentService? _service;
+        private IContainerService? _serviceContainer;
+
+
+        private static readonly HttpClient client = new HttpClient();
+
+        private string requestUri = "https://localhost:7153/api/Contents/";
+
+        public ContentsController(IContentService service, IContainerService serviceContainer)
+        {
+            _service = service ?? throw new ArgumentNullException(nameof(service));
+            _serviceContainer = serviceContainer ?? throw new ArgumentNullException(nameof(serviceContainer));
+
+            client.DefaultRequestHeaders.Accept.Clear();
+
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+            client.DefaultRequestHeaders.Add("User-Agent", "Jim's API");
+        }
+
+        // Example: https://localhost:7153/api/Contents
+        public async Task<IActionResult> Index()
+        {
+            var response = await _service.FindAll();
+
+            return View(response);
+        }
+
+        // GET: Content/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var content = await _service.FindOne(id);
+            if (content == null)
+            {
+                return NotFound();
+            }
+
+            return View(content);
+        }
+
+        // GET: Content/Create
+        public async Task<IActionResult> Create()
+        {
+            var response = await _serviceContainer.FindAll();
+            ViewData["ContainerId"] = new SelectList(response, "Id", "Description");
+            return View();
+        }
+
+        // POST: Content/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create([Bind("Id,Quantity,Description,containerId")] Content content)
+        public async Task<IActionResult> Create(Content content)
+        {
+            content.Id = null;
+            var resultPost = await client.PostAsync<Content>(requestUri, content, new JsonMediaTypeFormatter());
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Content/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var response = await _serviceContainer.FindAll();
+            ViewData["ContainerId"] = new SelectList(response, "Id", "Description");
+
+            var content = await _service.FindOne(id);
+            if (content == null)
+            {
+                return NotFound();
+            }
+
+            return View(content);
+        }
+
+        // POST: Content/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("Id,Quantity,Description,containerId")] Content content)
+        public async Task<IActionResult> Edit(int id, Content content)
+
+        {
+            if (id != content.Id)
+            {
+                return NotFound();
+            }
+
+            var resultPut = await client.PutAsync<Content>(requestUri + content.Id.ToString(), content, new JsonMediaTypeFormatter());
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: Content/Delete/5
+        public async Task<IActionResult> Delete(int id)
+
+        {
+            var content = await _service.FindOne(id);
+            if (content == null)
+            {
+                return NotFound();
+            }
+
+            return View(content);
+        }
+
+        // POST: Content/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var resultDelete = await client.DeleteAsync(requestUri + id.ToString());
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
